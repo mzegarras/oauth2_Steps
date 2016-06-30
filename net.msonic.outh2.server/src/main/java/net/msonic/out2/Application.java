b9d07cf0b495467c19268c9df1568ddf777e236b
manuelzegarra <mzegarra@gmail.com>
2016-06-29 20:03:44 -0500

    User in databases

--- net.msonic.outh2.server/src/main/java/net/msonic/out2/Application.java
+++ net.msonic.outh2.server/src/main/java/net/msonic/out2/Application.java
@@ -79,16 +79,28 @@ public class Application {
 		@Override
 		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 			// @formatter:off
-			clients.inMemory().withClient("my-trusted-client")
-					.authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
-					.authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT").scopes("read", "write", "trust")
-					.resourceIds("sparklr").accessTokenValiditySeconds(60).and()
-					.withClient("my-client-with-registered-redirect").authorizedGrantTypes("authorization_code")
-					.authorities("ROLE_CLIENT").scopes("read", "trust").resourceIds("sparklr")
-					.redirectUris("http://anywhere?key=value").and().withClient("my-client-with-secret")
-					.authorizedGrantTypes("client_credentials", "password").authorities("ROLE_CLIENT").scopes("read")
-					.resourceIds("sparklr").secret("secret");
-             
+			
+			clients.jdbc(dataSource);  // If you want to maintain client details is database
+			
+			/*
+			clients.inMemory().
+					withClient("my-trusted-client")
+						.authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+						.authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+						.scopes("read", "write", "trust")
+						.resourceIds("sparklr").accessTokenValiditySeconds(60).and()
+					.withClient("my-client-with-registered-redirect")
+						.authorizedGrantTypes("authorization_code")
+						.authorities("ROLE_CLIENT")
+						.scopes("read", "trust")
+						.resourceIds("sparklr")
+						.redirectUris("http://anywhere?key=value").and()
+					.withClient("my-client-with-secret")
+						.authorizedGrantTypes("client_credentials", "password")
+						.authorities("ROLE_CLIENT")
+						.scopes("read")
+						.resourceIds("sparklr").secret("secret");
+             */
 			// @formatter:on
 		}
 
